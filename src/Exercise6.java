8eb5e2036747ea6f5e0e1814bb01030cd601ea73
Mohammed Faiz <mm2046@hw.ac.uk>
2023-08-08 21:03:26 +0400

    Upto Ex 7 complete

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -4,10 +4,23 @@
  */
 public class Exercise6 {
 
+	private static final String Final = null;
+
 	public static void main(String[] args) {
 		/* First, copy across the code from your
 		   solution to Exercise 4. */
-
+		
+		// Assign variables
+		int age = 18;
+		final int Days = 365*age;
+		final int Hours = 24*Days;
+		final int Minutes = 60*Hours;
+		
+		
+		// Print them out
+		System.out.println("Days: " + Days);
+		System.out.println("Hours: " + Hours);
+		System.out.println("Minutes: " + Minutes);
 	}
 
 }
--- src/Exercise7.java
+++ src/Exercise7.java
@@ -1,12 +1,32 @@
+
 /**
  * Week 2, Exercise 7.
  * @author INSERT YOUR NAME HERE
  */
+import java.util.Scanner;
+
 public class Exercise7 {
 
 	public static void main(String[] args) {
-		/* First, copy across the code from your
-		   solution to Exercise 6. */
+		/*
+		 * First, copy across the code from your solution to Exercise 6.
+		 */
+		// Assign variables
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter your age: ");
+		int age = scan.nextInt();
+
+		final int Days = 365 * age;
+		final int Hours = 24 * Days;
+		final int Minutes = 60 * Hours;
+
+		// Print them out
+
+		System.out.println(
+		"You are " + age + " years old, \n which is the same as "
+		+ Days + " Days, \n which is the same as "
+		+ Hours + " Hours,\n which is" + " the same as " 
+		+ Minutes + " Minutes.");
 
 	}
 
