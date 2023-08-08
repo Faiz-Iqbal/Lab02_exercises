3f980ff0baa15458e2142b0f93457bc89e0b03da
Mohammed Faiz <mm2046@hw.ac.uk>
2023-08-08 17:28:58 +0400

    Variables Created

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -6,7 +6,9 @@ public class Exercise1 {
 
 	public static void main(String[] args) {
 		// Declare and assign variables just below here
-
+		int Days = 365;
+		int Hours = 24;
+		int Minutes = 60;
 		
 		// Then add code to print them out below here
 		
